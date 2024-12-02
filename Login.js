f74a837a4570fa5c91b1855b6514980acbca1f2b
Marinalva0 <164238004+marinalva0@users.noreply.github.com>
2024-12-02 14:45:26 -0300

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,11 @@
+function login(){
+    window.location
+    console.log("### window", window);
+    console.log("### window.location", window.location);
+}
+function register(){
+    window.location.href = "contato/Admin/Mensagem.html";
+}
+function inserirMensagem(){
+    var imagem = "imagem"
+}
